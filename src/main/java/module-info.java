1ad79974b3e68cdf0b41d44b2bacc2c33d868443
Burak <myburakoz@gmail.com>
2026-01-01 18:07:44 +0300

    The place of Database seeder changed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,12 +9,12 @@ module com.greengrocer {
     requires io;
 
     opens com.greengrocer to javafx.fxml;
-    // opens com.localgreengrocer.controllers to javafx.fxml;
-    // opens com.localgreengrocer.models to javafx.base;
+    opens com.localgreengrocer.controllers to javafx.fxml;
+    opens com.localgreengrocer.models to javafx.base;
 
     exports com.greengrocer;
-    // exports com.localgreengrocer.controllers;
-    // exports com.localgreengrocer.models;
-    // exports com.localgreengrocer.dao;
-    // exports com.localgreengrocer.utils;
+    exports com.localgreengrocer.controllers;
+    exports com.localgreengrocer.models;
+    exports com.localgreengrocer.dao;
+    exports com.localgreengrocer.utils;
 }
